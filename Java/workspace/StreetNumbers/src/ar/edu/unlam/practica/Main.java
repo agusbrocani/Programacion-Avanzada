dbda729e99466c90c2c3ffccaea2e11c4ac1c1b8
agusbrocani <aguss.brocani@gmail.com>
2025-08-23 18:06:41 -0300

    Inicializo repo

---
+++ Java/workspace/StreetNumbers/src/ar/edu/unlam/practica/Main.java
@@ -0,0 +1,7 @@
+package ar.edu.unlam.practica;
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.println(StreetNumbers.cuadratica(8));
+	}
+}
---
+++ Java/workspace/StreetNumbers/src/ar/edu/unlam/practica/StreetNumbers.java
@@ -0,0 +1,13 @@
+package ar.edu.unlam.practica;
+
+public class StreetNumbers {
+	public static int cuadratica(int n) {
+
+	}
+//	int lineal(int n) {
+//		
+//	}
+//	int constante(int n) {
+//		
+//	}
+}
